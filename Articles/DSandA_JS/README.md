0625192b262c0cc8118424274038998d2ec505cd
Cygra <sjtuwbh@gmail.com>
2018-12-31 12:49:21 +0800

    云里帝城双凤阙，雨中春树万人家。

---
+++ Articles/DSandA_JS/README.md
@@ -0,0 +1,6 @@
+数据结构与算法 JavaScript 描述
+
+[美]Michael McMillan 著
+王群锋 杜欢 译
+
+人民邮电出版社 2014 年 9 月第 1 版
---
+++ Articles/DSandA_JS/chap_2.js
@@ -0,0 +1,51 @@
+Array.matrix = (numRows, numCols, initial) => {
+  let arr = []
+  for (let i = 0; i < numRows; ++i) {
+    let cols = []
+    for(let j = 0; j < numCols; ++j) {
+      cols[j] = initial
+    }
+    arr[i] = cols
+  }
+  return arr
+}
+
+console.log(Array.matrix(3,3,''))
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+let p1 = new Point(1,2)
+let p2 = new Point(3,4)
+let pointArr = [p1, p2]
+console.log(pointArr)
+
+class weekTemps {
+  constructor() {
+    this.dataStore = []
+  }
+
+  add(temp) {
+    this.dataStore.push(temp)
+  }
+
+  averge() {
+    let total = 0
+    const len = this.dataStore.length
+    for (let i = 0; i < len; ++i) {
+      total += this.dataStore[i]
+    }
+    return total / len
+  }
+}
+
+let thisWeek = new weekTemps()
+thisWeek.add(1)
+thisWeek.add(2)
+thisWeek.add(3)
+thisWeek.add(5)
+console.log(thisWeek.averge())
