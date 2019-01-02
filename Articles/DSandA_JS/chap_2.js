9cec6426bc7f1f122dacc42cf2ed4c3c61b1a17e
Cygra <sjtuwbh@gmail.com>
2019-01-02 22:21:54 +0800

    军合力不齐，踌躇而雁行。

--- Articles/DSandA_JS/chap_2.js
+++ Articles/DSandA_JS/chap_2.js
@@ -1,3 +1,7 @@
+// 数组在 JavaScript 中只是一种特殊的对象，所以效率上不如其他语言的数组高。
+
+// https://github.com/Cygra/Cygra_la_Artis/issues/11
+
 Array.matrix = (numRows, numCols, initial) => {
   let arr = []
   for (let i = 0; i < numRows; ++i) {
--- Articles/DSandA_JS/chap_2.md
+++
@@ -1,5 +0,0 @@
-# 数组
-
-数组在 JavaScript 中只是一种特殊的对象，所以效率上不如其他语言的数组高。
-
-https://github.com/Cygra/Cygra_la_Artis/issues/11
